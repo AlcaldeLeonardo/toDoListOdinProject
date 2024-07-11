2dd092724b79803d95ca0fcf356cd76b2972aeb4
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-07-11 01:43:52 -0300

    basic style app done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import './style.scss'
+import './css/reset.css'
+import './scss/style.scss'
 import { renderer } from './views/renderer'
 import { titleH2DOM } from './views/titleDOM'
 
-renderer(titleH2DOM("Hola"))
+// renderer(titleH2DOM("Hola"))
