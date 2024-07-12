85c78fb0ce31154dad990e076ca5284905667ab0
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-07-12 17:33:01 -0300

    btns Classes created
    - These can be used in the appendChild method or assigned to a variable to add to the parent addEventListener or be modified as desired.

---
+++ src/views/atomViews/btnsDOM.js
@@ -0,0 +1,24 @@
+export function plusBtnDOM(){
+  const btn = btnDOM("+", "main__addToDo btn", "addBtn")
+
+  return btn;
+}
+
+export function editBtn() {
+  const btn = btnDOM("Edit", "btn btn--primary", "editBtn")
+  
+  return btn;
+}
+export function delBtn() {
+  const btn = btnDOM("Delete", "btn btn--red", "editBtn")
+  return btn;
+}
+
+function btnDOM(nameBtn = "function", classCss = "btn", id = ""){
+  const btn = document.createElement("button")
+  btn.innerHTML = nameBtn;
+  if(id) btn.id = id;
+  btn.className = classCss;
+
+  return btn;
+}
