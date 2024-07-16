d374cc221b66e59f1863fc772aa958e10a113124
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-07-16 17:18:35 -0300

    litle fix at imports

--- src/views/updateScreen.js
+++ src/views/updateScreen.js
@@ -2,8 +2,8 @@ import { parallaxFX } from "./parallaxFX";
 import { asideDOM } from "./moleculeViews/asideDOM";
 import { headerDOM } from "./moleculeViews/headerDOM";
 import { mainDOM } from "./moleculeViews/mainDOM";
-import { addTaskModal, modalContainer } from "./moleculeViews/modalContainer";
 import { renderer } from "./renderer";
+import { modalContainer } from "./moleculeViews/modalContainer";
 
 export function updateScreen(ActiveColeccion, modal = null) {
   const root = document.querySelector("#root")
