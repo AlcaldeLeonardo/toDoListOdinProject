f75d4db5ffae2a70eade8fa49dabea0498a95732
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-07-17 21:50:43 -0300

    unneccesary btn are now deleted

--- src/views/moleculeViews/buttonBoxDOM.js
+++ src/views/moleculeViews/buttonBoxDOM.js
@@ -5,8 +5,8 @@ export function buttonBoxDOM(){
 
   const buttonBox = document.createElement(`div`);
   buttonBox.className = "toDoCard__buttonBox buttonBox"
-  buttonBox.appendChild(editBtn());
   buttonBox.appendChild(delBtn());
 
   return buttonBox;
 }
+
