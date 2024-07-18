270efe7eb74b4e4bcb9ca943bafbf17b1460a661
Leonardo Esteban Alcalde Gómez <108157788+AlcaldeLeonardo@users.noreply.github.com>
2024-07-18 04:06:29 -0300

    split array metod in deleteTask() implemented

--- src/modules/classes/TasksCollection.js
+++ src/modules/classes/TasksCollection.js
@@ -1,24 +1,27 @@
-export class TasksCollection{
-    static staticId = 0;
+export class TasksCollection {
+  static staticId = 0;
 
-    constructor(title,tasksArray = []){
-        this.id = TasksCollection.staticId
-        this.title = title
-        this.tasksArray = tasksArray;
-        TasksCollection.staticId++;
-    }
+  constructor(title, tasksArray = []) {
+    this.id = TasksCollection.staticId;
+    this.title = title;
+    this.tasksArray = tasksArray;
+    TasksCollection.staticId++;
+  }
 
-    get Title(){
-        return this.title;
-    }
-    get TasksArray(){
-        return this.tasksArray;
-    }
-    addTask(task){
-        this.tasksArray.push(task)
-    }
+  get Title() {
+    return this.title;
+  }
+  get TasksArray() {
+    return this.tasksArray;
+  }
+  addTask(task) {
+    this.tasksArray.push(task);
+  }
 
-    deleteTask(taskId){
-        this.tasksArray = this.tasksArray.filter(obj => obj.id !== taskId);
+  deleteTask(taskId) {
+    const index = this.tasksArray.findIndex((obj) => obj.id === taskId);
+    if (index !== -1) {
+      this.tasksArray.splice(index, 1);
     }
+  }
 }
